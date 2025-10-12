6c0fbf9b376a739db3fab61b1c114d2ccd43f553
A.Ivanov <sasha1_00@mail.ru>
2025-10-12 16:18:28 +0300

    Second try

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -8,7 +8,7 @@ class RegistrationPage:
         pass
 
     def open(self):
-        browser.open('/automation-practice-form')
+        browser.open('https://demoqa.com/automation-practice-form')
         browser.all('[id^=google_ads][id$=container__]').with_(timeout=10).wait_until(
             have.size_greater_than_or_equal(3)
         )
